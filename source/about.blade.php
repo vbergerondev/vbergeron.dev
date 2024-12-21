2e98365b6036cfa93ae39f8013b7c7741e583295
Vincent Bergeron <vincent_bergeron98@hotmail.com>
2024-12-20 20:40:06 -0500

    Updates

--- source/about.blade.php
+++ source/about.blade.php
@@ -1,26 +1,20 @@
 ---
 title: About
-description: A little bit about the site
+description: About me
 ---
 @extends('_layouts.main')
 
 @section('body')
     <h1>About</h1>
 
-    <img src="/assets/img/vincent.png"
-        alt="About image"
-        class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
-
     <p class="mb-6">
-        Hey there! I'm Vincent Bergeron. Coding has been my passion for over a decade now. It all began back in 2013 when I started with basic HTML, but I quickly found my stride with PHP, which just clicked for me. Around the same time, I enrolled in an OpenClassrooms course (then known as Site du Zéro) to really master PHP & MySQL.
-    </p>
-
-    <p class="mb-6">
-        As a young developer, I eagerly followed my favorite YouTuber's tutorials, diving into frameworks like Rails and CakePHP. Then, in 2017, while working on a school project, I discovered Laravel and was immediately captivated by its power and elegance. The project also pushed me to learn Vue.js. Since then, I've been hooked. This journey solidified my passion for modern web development tools, guiding me towards specializing in Laravel.
-    </p>
-
-    <p class="mb-6">
-        In 2018, I joined my current company, where I've embraced continuous learning and adopted best practices in software development. Leading teams and tackling complex projects in Laravel and Rails have been incredibly rewarding, allowing me to apply my passion for solving intricate problems through meticulous coding.
+        My name is Vincent. I'm a 26-year-old father of two, living in Québec, Canada.
+        <br><br>
+        From a young age, I was fascinated by computers and video games. It was always clear to me that I wanted to create websites for a living. Back in 2013, while still in high school, I decided it was time to start learning on my own through YouTube tutorials and online courses. I began with the basics of HTML, CSS, and JavaScript, quickly advancing to PHP and MySQL.
+        <br><br>
+        In 2016–2017, I attended <a href="https://www.multihexa.quebec/" target="_blank">Collège Multihexa</a>, where I was introduced to Laravel and Vue.js. While I already had experience with frameworks like CodeIgniter, CakePHP, and Ruby on Rails, discovering Laravel and its ecosystem was a transformative moment. I became deeply passionate about Laravel and have made it a priority to stay up-to-date with its developments every single day since then.
+        <br><br>
+        Fast forward to now: I've been working at the same company for 7 years, gaining extensive experience building a wide variety of Laravel applications. One of the most rewarding aspects of my role has been mentoring junior developers and interns, helping them get started with Laravel and guiding them as they grow. Sharing my passion for the framework and watching others develop their skills has been an incredibly fulfilling part of my journey.
     </p>
 
     <h3 class="mt-20">Skills</h3>
